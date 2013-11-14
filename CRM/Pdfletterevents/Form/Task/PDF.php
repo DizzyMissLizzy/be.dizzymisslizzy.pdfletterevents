28c0ed9868ee45ef6a01d0f430023abbe50a845b
Lies Lamoen <lies.lamoen@mediaraven.be>
2013-11-14 11:56:05 +0100

    Event tokens added

---
+++ CRM/Pdfletterevents/Form/Task/PDF.php
@@ -0,0 +1,110 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.4                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2013                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ *
+ * @package CRM
+ * @copyright CiviCRM LLC (c) 2004-2013
+ * $Id$
+ *
+ */
+
+/**
+ * This class provides the functionality to create PDF letter for a group of
+ * contacts or a single contact.
+ */
+class CRM_Pdfletterevents_Form_Task_PDF extends CRM_Event_Form_Task {
+
+  /**
+   * all the existing templates in the system
+   *
+   * @var array
+   */
+  public $_templates = NULL;
+
+  public $_single = NULL;
+
+  public $_cid = NULL;
+
+  /**
+   * build all the data structures needed to build the form
+   *
+   * @return void
+   * @access public
+   */
+  function preProcess() {
+    $this->skipOnHold = $this->skipDeceased = FALSE;
+    parent::preProcess();
+    $this->setContactIDs();
+    CRM_Contact_Form_Task_PDFLetterCommon::preProcess($this);
+  }
+
+  /**
+   * Set defaults
+   * (non-PHPdoc)
+   * @see CRM_Core_Form::setDefaultValues()
+   */
+  function setDefaultValues() {
+    return  CRM_Contact_Form_Task_PDFLetterCommon::setDefaultValues();
+  }
+
+  /**
+   * Build the form
+   *
+   * @access public
+   *
+   * @return void
+   */
+  public function buildQuickForm() {
+    //enable form element
+    $this->assign('suppressForm', FALSE);
+    CRM_Contact_Form_Task_PDFLetterCommon::buildQuickForm($this);
+  }
+
+  /**
+   * list available tokens, at time of writing these were
+   * {event.event_id} => Event ID'
+   * {event.title} => Event Title
+   * {event.start_date} => Event Start Date
+   * {event.end_date} => Event End Date
+   * {event.event_type} => Event Type
+   * {event.summary} => Event Summary
+   * {event.description} => Event Description
+   * {event.contact_email} => Event Contact Email
+   * {event.contact_phone} => Event Contact Phone
+   * {event.location} => Event Location
+   * {event.description} => Event Description
+   * {event.location} => Event Location
+   * {event.fee_amount} => Event Fees
+   * {event.info_url} => Event Info URL
+   * {event.registration_url} => Event Registration URL
+   * @return Ambigous <NULL, multitype:string Ambigous <string, string> >
+   */
+  public function listTokens() {
+    return CRM_Core_SelectValues::eventTokens();
+  }
+}
