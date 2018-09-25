96168b77ee27b47b1948892bc1faa08a0fd84399
Johan Vervloet <johan.vervloet@gmail.com>
2018-09-25 11:48:12 +0200

    Headless test.

--- tests/phpunit/CRM/Pdfletterevents/SelectValuesParticipantTokenSetTest.php
+++ tests/phpunit/CRM/Pdfletterevents/SelectValuesParticipantTokenSetTest.php
@@ -1,32 +1,54 @@
 <?php
 
-/**
- * This is a generic test class for the extension (implemented with PHPUnit).
- */
-class CRM_Pdfletterevents_SelectValuesParticipantTokenSetTest extends PHPUnit_Framework_TestCase {
+use CRM_Pdfletterevents_ExtensionUtil as E;
+use Civi\Test\HeadlessInterface;
+use Civi\Test\HookInterface;
+use Civi\Test\TransactionalInterface;
 
-  /**
-   * The setup() method is executed before the test is executed (optional).
-   */
+/**
+ * FIXME - Add test description.
+ *
+ * Tips:
+ *  - With HookInterface, you may implement CiviCRM hooks directly in the test class.
+ *    Simply create corresponding functions (e.g. "hook_civicrm_post(...)" or similar).
+ *  - With TransactionalInterface, any data changes made by setUp() or test****() functions will
+ *    rollback automatically -- as long as you don't manipulate schema or truncate tables.
+ *    If this test needs to manipulate schema or truncate tables, then either:
+ *       a. Do all that using setupHeadless() and Civi\Test.
+ *       b. Disable TransactionalInterface, and handle all setup/teardown yourself.
+ *
+ * @group headless
+ */
+class CRM_Pdfletterevents_SelectValuesParticipantTokenSetTest extends \PHPUnit_Framework_TestCase implements HeadlessInterface, HookInterface, TransactionalInterface {
+
+  public function setUpHeadless() {
+    // Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
+    // See: https://github.com/civicrm/org.civicrm.testapalooza/blob/master/civi-test.md
+    return \Civi\Test::headless()
+      ->installMe(__DIR__)
+      ->apply();
+  }
+
   public function setUp() {
     parent::setUp();
   }
 
-  /**
-   * The tearDown() method is executed after the test was executed (optional)
-   * This can be used for cleanup.
-   */
   public function tearDown() {
     parent::tearDown();
   }
 
   /**
-   * Simple example test case.
-   *
-   * Note how the function name begins with the word "test".
+   * Example: Test that a version is returned.
    */
-  public function testExample() {
-    self::assertTrue(TRUE, "The argument must be true to pass the test");
+  public function testWellFormedVersion() {
+    $this->assertRegExp('/^([0-9\.]|alpha|beta)*$/', \CRM_Utils_System::version());
+  }
+
+  /**
+   * Example: Test that we're using a fake CMS.
+   */
+  public function testWellFormedUF() {
+    $this->assertEquals('UnitTests', CIVICRM_UF);
   }
 
 }
