13ff4462ab6a0c933a98bcf91acd08eb5b5d20a4
Johan Vervloet <johan.vervloet@gmail.com>
2018-10-02 21:35:56 +0200

    Simplify registration of token subscriber.
    I removed the test that was causing troubles because of some
    CiviCRM core oddity.

--- pdfletterevents.php
+++ pdfletterevents.php
@@ -15,18 +15,13 @@ function pdfletterevents_civicrm_config(&$config) {
   if (isset(Civi::$statics[__FUNCTION__])) { return; }
   Civi::$statics[__FUNCTION__] = 1;
 
+  $subscriber = new CRM_Pdfletterevents_ParticipantTokenSubscriber(
+    new CRM_Pdfletterevents_ParticipantTokenSet()
+  );
+
   Civi::dispatcher()->addListener(
     'civi.token.eval',
-    function (TokenValueEvent $e) {
-      // Only create this subscriber when it is needed, because it uses
-      // a static class that caches the token values.
-      // See CRM_Core_SelectValues::participantTokens().
-      // If the subscriber is instantiated at the time hook_civicrm_config is
-      // called, the caching causes a unit test to fail :-(((
-      $subscriber = new CRM_Pdfletterevents_ParticipantTokenSubscriber(
-        new CRM_Pdfletterevents_ParticipantTokenSet()
-      );
-
+    function (TokenValueEvent $e) use ($subscriber) {
       $subscriber->evaluateTokens($e);
     }
   );
